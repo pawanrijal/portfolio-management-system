63bfbf80c6a841a759aa84a5ed1f20c719c3b3d3
Pawan Rijal <pawan.rijal@texasintl.edu.np>
2022-06-02 17:01:19 +0545

    Connected to database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Listening to the server");
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+const { sequelize } = require("./lib/databaseConnection");
+
+//sequelize authentication to database
+sequelize
+  .authenticate()
+  .then(() => {
+    // sequelize.sync({ alter: true });
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//Test
+app.get("/test", (req, res) => {
+  res.send({ message: "Test Successful" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to the server on ${PORT}`);
 });
---
+++ server/lib/databaseConnection.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require("sequelize");
+
+//DB SETTINGS
+const DB_Name = process.env.DB_Name;
+const DB_username = process.env.DB_Username;
+const DB_password = process.env.DB_Password;
+const DB_port = process.env.DB_PORT;
+const db = {};
+
+const sequelize = new Sequelize(DB_Name, DB_username, DB_password, {
+  host: process.env.DB_Host,
+  dialect: process.env.DB_Dialect,
+  port: DB_port,
+  pool: {
+    max: 20,
+    idle: 30000,
+    min: 5,
+  },
+  define: {
+    underscored: true,
+  },
+});
+
+db.sequelize = sequelize;
+module.exports = db;
