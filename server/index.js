8a632269769c63035655027885854aee2156b5fc
Pawan Rijal <pawan.rijal@texasintl.edu.np>
2022-06-02 17:05:08 +0545

    added global error handler

--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 const { sequelize } = require("./lib/databaseConnection");
+const HttpException = require("../exceptions/HttpException");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const { router } = require("./routes");
+// app.use(router);
 
 //sequelize authentication to database
 sequelize
@@ -21,6 +30,28 @@ sequelize
 //Test
 app.get("/test", (req, res) => {
   res.send({ message: "Test Successful" });
+});
+
+//if Routes Not Found
+app.use((req, res, next) => {
+  const err = new HttpException(404, "Route doesnot exist");
+
+  next(err);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  err.success = false;
+  err.status = err.status || 500;
+  err.message = err.message || "Something went wrong";
+  err.data = err.data || err.stack;
+
+  res.status(err.status).json({
+    success: err.success,
+    status: err.status,
+    message: err.message,
+    data: err.data,
+  });
 });
 
 app.listen(PORT, () => {
